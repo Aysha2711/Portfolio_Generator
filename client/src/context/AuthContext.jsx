1486af5f0fb857798717f065e7f092a841331475
Aysha2711 <fathima.aysha2711@gmail.com>
2026-04-09 10:03:09 +0530

    URL issue solved

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,6 +1,8 @@
 import React, { createContext, useState } from 'react';
 import axios from 'axios';
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
@@ -9,7 +11,7 @@ export const AuthProvider = ({ children }) => {
     return savedUser ? JSON.parse(savedUser) : null;
   });
 
-  const apiUrl = 'http://localhost:3000/api/auth'; //  this matches backend URL
+  const apiUrl = `${API_URL}/api/auth`;
 
   const login = async (email, password) => {
     const res = await axios.post(`${apiUrl}/login`, { email, password });
